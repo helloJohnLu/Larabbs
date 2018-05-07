9b57a4d0de29564b4a8cc744079fd1a3300ae268
John Lu <johnluchn@gmail.com>
2018-05-07 09:28:58 +0000

    话题列表

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,18 @@ namespace App\Models;
 
 class Topic extends Model
 {
+    // 可写入字段
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    // 话题与分类，一对一关联
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // 话题与用户，一对一关联
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
