e35a66e8bf6211be58af78e027ecc61419fe8a97
John Lu <johnluchn@gmail.com>
2018-05-07 09:02:21 +0000

    填充话题与用户测试数据

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // 可写入字段
+    protected $fillable = ['name', 'description'];
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::post('password.reset', 'Auth\ResetPasswordController@reset');
 // 用户个人中心
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
