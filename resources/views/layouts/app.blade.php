9340a332fc108a432463f3600add35b7975ef701
John Lu <johnluchn@gmail.com>
2018-05-08 02:28:19 +0000

    显示帖子

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="@yield('description', 'LaraBBS —— Laravel 爱好者社区')">
 
     {{-- CSRF Tokdn--}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
