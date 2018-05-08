c771b6b844f8dd43f95a0229f197f7c6dda0ea95
John Lu <johnluchn@gmail.com>
2018-05-08 09:27:43 +0000

    XSS 安全漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,9 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        // 过滤内容数据，防止XSS防止
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
