36ad4aa715b5be089de3b0835c3ef5358afec14b
John Lu <johnluchn@gmail.com>
2018-05-19 09:58:31 +0000

    消息通知，生成通知类 & 触发通知

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,20 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    // 发通知的方法
+    public function notify ($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了
+        if ($this->id == \Auth::id()) {
+            return ;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
