f652df1f2ed63798a9892cc2143bafeb42ac30d3
John Lu <johnluchn@gmail.com>
2018-05-21 10:29:28 +0000

    角色权限管理，后台访问权限

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,6 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 // 消息通知
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+// 无权访问后台
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
